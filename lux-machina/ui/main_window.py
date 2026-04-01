ee2b5acf14d46c0b2ce3a5109b06dbf13bf4753b
François Margall <fr.margall@proton.me>
2026-04-01 10:01:25 +0200

    ✨ feat(ui): add main window

---
+++ lux-machina/ui/main_window.py
@@ -0,0 +1,22 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("LuxMachina")
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.showMaximized()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
