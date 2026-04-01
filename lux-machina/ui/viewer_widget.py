ba1383c428a0811e1411366b21e24e5a5ed9e419
François Margall <fr.margall@proton.me>
2026-04-01 10:50:52 +0200

    ✨ feat(ui): add RGB support for viewer widget

--- lux-machina/ui/viewer_widget.py
+++ lux-machina/ui/viewer_widget.py
@@ -18,13 +18,19 @@ class ViewerWidget(QLabel):
         # Normalize the image to the range [0, 255]
         if image.dtype != np.uint8:
             image = (np.clip(image, 0.0, 1.0) * 255).astype(np.uint8)
-            h, w  = image.shape[:2]
+        
+        h, w  = image.shape[:2]
 
         # Build QImage
-        qImage = QImage(image.data, w, h, image.strides[0], QImage.Format_Grayscale8)
+        if image.ndim == 2: # Gray scale
+            qImage = QImage(image.data, w, h, image.strides[0], QImage.Format_Grayscale8)
+        elif image.ndim == 3 and image.shape[2] == 3: # RGB
+            qImage = QImage(image.data, w, h, image.strides[0], QImage.Format_RGB888)
+        else:
+            raise ValueError("Unsupported image format: expected 2D grayscale or 3D RGB image")
 
         # Keep a reference to the QImage to prevent it from being garbage collected
-        self.qImage = qImage
+        self._qImage = qImage
 
         # Show the image
         pixmap = QPixmap.fromImage(qImage)
