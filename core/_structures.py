3daed7ad68389067c8cfde6e881183404b9f2b41
François Margall <fr.margall@proton.me>
2026-05-19 16:53:46 +0200

    ✨ feat(_structures): add initial structures

---
+++ core/_structures.py
@@ -0,0 +1,49 @@
+import warp as wp
+
+
+@wp.struct
+class Ray:
+    origin    : wp.vec3f
+    direction : wp.vec3f
+    throughput: wp.float32
+    pdf       : wp.float32
+    wavelength: wp.float32
+
+    isAlive   : wp.bool
+    depth     : wp.int32
+
+@wp.struct
+class Material:
+    iorPositive: wp.float32
+    iorNegative: wp.float32
+
+@wp.struct
+class Primitive:
+    type      : wp.int32
+    materialID: wp.int32
+    v0        : wp.vec3f
+    v1        : wp.vec3f
+    v2        : wp.vec3f
+    v3        : wp.vec3f
+
+@wp.struct
+class Intersection:
+    t          : wp.float32
+    normal     : wp.vec3f
+    primitiveID: wp.int32
+
+@wp.struct
+class LightSource:
+    type       : wp.int32
+    primitiveID: wp.int32
+    power      : wp.float32
+
+@ wp.struct
+class Sensor:
+    type: wp.int32
+    v0  : wp.vec3f
+    v1  : wp.vec3f
+    v2  : wp.vec3f
+    v3  : wp.vec3f
+    i0  : wp.int32
+    i1  : wp.int32
